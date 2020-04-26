6d435220c8063699350121a0925d7e3110001bbb
Lorenzo Benfenati <lorenzo.benfenati@spekno.eu>
2020-04-26 16:49:10 +0200

    prima di rifare tutto il message passing

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,9 +1,22 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.context == "token" ){
     window.token = request.token;
+    window.email = request.email;
   }
 })
 
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.create({url: 'popup.html'})
 })
+
+/*chrome.tabs.sendMessage(tab.id, {content: "message"}, function(response) {
+ 	if(response) {
+ 		//We do something
+ 	}
+});*/
+$.getJSON("https://t2spekno-62a66c37.northeurope.cloudapp.azure.com/api/Links/1",
+          {},
+          function(data) {
+            console.log(data);
+          }
+        );
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,33 +1,31 @@
-$(document).ready(function() {$('head').append('<style> .tt-link-box { '+
-      '  background: #ffffff;'+
-      '  width: 100px;'+
-      '  height: 50px;'+
-        '  padding: 5px;'+
-      '  display: none;'+
-      '  float: right;position: absolute;z-index: 100;'+
+$(document).ready(function() {$('head').append('<style> .tt-link-box { background: transparent; width: 55px; height: 27px; padding: 5px; display: none; float: right; position: absolute; z-index: 100;'+
       "}</style>");
-      $('.tt-toggle').click(function() {$(this).parent().children('.tt-link-box').toggle('slow');console.log('qui') });
+    //  $('.tt-toggle').click(function() {$(this).parent().children('.tt-link-box').toggle('slow');console.log('qui') });
 });
-
+//const bg = chrome.extension.getBackgroundPage();
+var state ;
+var imgstr = "";
+var imgup =  chrome.runtime.getURL("images/tup.png");
+var imgdown =  chrome.runtime.getURL("images/tdown.png");
 $( ".r" ).each(function( index ) {
   console.log($(this).children("a").attr("href"));
+  var result = this;
   $.getJSON("https://t2spekno-62a66c37.northeurope.cloudapp.azure.com/api/Links/1",
             {url:$(this).children("a").attr("href")},
             function(data) {
               console.log(data);
+              state = data["state"];
+              console.log(state);
+              if (state == 'not_found' ) {
+                imgstr = chrome.runtime.getURL("images/grey.png");
+              }
+              else if (state ==1) {
+                imgstr = chrome.runtime.getURL("images/green.png");
+              }
+              else {
+                imgstr = chrome.runtime.getURL("images/red.png");
+              }
+              $(result).append('<div style="display:contents;"><a class="tt-toggle"><img  src="'+imgstr+'"></a><div class="tt-link-box"><a class="tt-up" href="#"><img style="width:25px" src="'+imgup+'"></a><a class="tt-down" href="#"><img style="width:25px" src="'+imgdown+'"></a></div></div>')
             });
-  var imgstr = "";
-  var imgup =  chrome.runtime.getURL("images/tup.png");
-  var imgdown =  chrome.runtime.getURL("images/tdown.png");
-  var rr = Math.floor(Math.random() * 3);
-  if (rr == 0 ) {
-    imgstr = chrome.runtime.getURL("images/yellow.png");
-  }
-  else if (rr==1) {
-    imgstr = chrome.runtime.getURL("images/green.png");
-  }
-  else {
-    imgstr = chrome.runtime.getURL("images/red.png");
-  }
-  $(this).append('<div style="display:contents;"><a class="tt-toggle"><img  src="'+imgstr+'"></a><div class="tt-link-box"><ul style="list-style-type: none;"><li><a class="tt-up" href="#"><img style="width:25px" src="'+imgup+'"></a></li><li><a class="tt-down" href="#"><img style="width:25px" src="'+imgdown+'"></a></li></div></div>')
 });
+$('.tt-toggle').click(function() {$(this).parent().children('.tt-link-box').toggle('slow');console.log('qui') });
